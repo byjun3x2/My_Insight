3c9e74823371af43151a93713099a7e4bcc7b021
byjun3x2 <byjun32@gmail.com>
2025-07-16 18:59:59 +0900

    feat: Add initial Backend SpringBoot Boiler Plate

---
+++ Backend/src/main/java/com/myinsight/backend/MyInsightBackendApplication.java
@@ -0,0 +1,13 @@
+package com.myinsight.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyInsightBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyInsightBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/myinsight/backend/MyInsightBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myinsight.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyInsightBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
